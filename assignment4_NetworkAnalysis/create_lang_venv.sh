0aab70cfa7183bd4e65a5a8e56d61abdd5861f7e
askesvane <askesvane@gmail.com>
2021-05-23 13:46:07 +0200

    script done

--- assignment4_NetworkAnalysis/create_lang_venv.sh
+++ assignment4_NetworkAnalysis/create_lang_venv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VENVNAME=network_environment
+VENVNAME=network_env
 
 python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
@@ -8,10 +8,8 @@ pip install --upgrade pip
 
 pip install ipython
 pip install jupyter
-python -m ipykernel install --user --name=$VENVNAME
 
 test -f requirements.txt && pip install -r requirements.txt
-python -m spacy download en_core_web_sm
 
 deactivate
 echo "build $VENVNAME"
--- assignment4_NetworkAnalysis/network.py
+++ assignment4_NetworkAnalysis/network.py
@@ -1,71 +1,40 @@
-
+#!/usr/bin/env python
 
 #_____________# Import packages #_____________# 
 
 # System tools
 import os
+import argparse
 
 # Data analysis
 import pandas as pd
-from collections import Counter
-from itertools import combinations 
-from tqdm import tqdm
-import argparse
-
-# NLP
-import spacy
-nlp = spacy.load("en_core_web_sm")
 
 # drawing
 import networkx as nx
 import matplotlib.pyplot as plt
 plt.rcParams["figure.figsize"] = (20,20)
 
-#_____________# The script #_____________# 
+#_____________# The script #_____________#
 
-# Function using argparse to get a filename from the commandline.
-def commandline_info():
-    ap = argparse.ArgumentParser(description='Creating a network. Please provide a filename to an edgelist stored as a csv-file in the data folder as well as a filter.')
-    ap.add_argument("-n", "--filename", required=True, type=str, help="str, the filename of any weighted edgelist stored as a csv-file")
-    ap.add_argument("-t", "--threshold", required=False, type=int, default=500, help="int, a threshold filtering by the weight of the edges. Default = 500")
-    
-    args = vars(ap.parse_args())
-    filename = args['filename']
-    threshold = args['threshold']
 
-    return(filename,threshold)
+### FUNCTIONS
 
+# function information
 def information(nx_object, name_kind):
+    """ 
+    Function to gather information from an nx object and store it in a pandas df that is returned.
+    """
     df = pd.DataFrame(nx_object) 
     df.columns =['Name', name_kind]
     return(df)
 
-
-def main():
-    
-    # Get the filename from the function filename()
-    filename, threshold = commandline_info()
-    #print(f"this is it {filename} and the treshold is {threshold}")
-
-    # Import the file specified in the commandline as a pd.
-    filepath = os.path.join("data",filename)
-    data = pd.read_csv(filepath)
-    
-    # Filter based on the weight of the edges
-    filtered = data[data["weight"] > threshold]
-    
-    
-    # Define outpath 
-    outpath_viz = os.path.join('viz',' network.png')
-    
-    # Create network
-    G = nx.from_pandas_edgelist(filtered, 'nodeA', 'nodeB', ["weight"])
-    nx.draw_shell(G, with_labels = True, font_weight= 'bold')
-    plt.savefig(outpath_viz, dpi=300, bbox_inches="tight")
-    
-    
-    # Create csv with relevant network information on each node
-    
+# function network_info
+def network_info(G):
+    """
+    Function that employs the information() function and gathers information on eigenvector, 
+    betweenness, and degree about the network. The function saves the gathered information as 
+    a csv-file in the folder 'output'.
+    """
     # Eigenvector. Calculate eigenvector centrality and save it as a df with meaningful column names
     eigenvector = nx.eigenvector_centrality(G)
     eigenvector = information(eigenvector.items(), "eigenvector")
@@ -83,17 +52,97 @@ def main():
     
     # Save as 'network_info.csv' in the folder 'output'
     network_info.to_csv("output/network_info.csv",index=False)
+    
+    return
 
+# Function network_plot
+def network_plot(layout, G):
+    """
+    Function that creates a network with the layout specified in the terminal. The network is saved in 
+    the folder 'viz' and is named after the specified layout.
+    """
+    # Define network image filename and path
+    network_filename = os.path.join('viz',f'network_{layout}.png')
+         
+    # Create a network with the layout options specified in the terminal (either random or shell)
+    if (layout == "random"):
+        # draw nx network plot
+        nx.draw_random(G, 
+                       with_labels = True,  
+                       node_color='red', 
+                       font_weight= 'bold', 
+                       node_size=1000, 
+                       edge_color = "grey")
+        
+    elif (layout == "shell"):
+        # draw nx network plot
+        nx.draw_shell(G, 
+                      with_labels = True,  
+                      node_color='red', 
+                      font_weight = 'bold', 
+                      node_size=1000, 
+                      edge_color = "grey")
+    
+    # Save the network to the folder 'viz' with the layout option in the filename
+    plt.savefig(network_filename, dpi=100, bbox_inches="tight")
+    return
 
+    
+### MAIN FUNCTION
+def main(args):
+    
+    # Import parameters specified in the terminal
+    filename = args["filename"]
+    threshold = args["threshold"]
+    layout = args["layout"]
+    
+    # Import the file specified in the commandline as a pd.
+    filepath = os.path.join("data",filename)
+    data = pd.read_csv(filepath)
+    
+    # Filter based on the weight of the edges
+    filtered = data[data['weight'] > threshold]
+    
+    # Print data reduction in the terminal
+    print(f"With a threshold of {threshold}, the data has been reduced from {str(len(data))} to {str(len(filtered))} entries.")
+       
+    # Create network object
+    G = nx.from_pandas_edgelist(filtered, 'nodeA', 'nodeB', ["weight"], create_using=nx.Graph())
+    
+    # Create a network with the layout specified in the terminal and save it in the 'viz' folder.
+    network_plot(layout, G)
+    
+    # Create df with relevant network information on each node and save it as a csv file
+    network_info(G)
+    
+    # Print information to the terminal
+    print(f"The network with a '{layout}' layout has been saved in the folder 'viz'. Relevant network information has been saved in the folder 'output'.")     
+
+    
+# RUN THE MAIN FUNCTION
 if __name__=="__main__":
-    main()
+    
+    # Argument parser
+    ap = argparse.ArgumentParser()
+    
+    ap.add_argument("-f", "--filename", type = str, default = "edgelist_weighted.csv",
+                    help = "Specify the filename of the data.")
+    ap.add_argument("-t", "--threshold", type = int, default = 500,
+                    help = "Specify the threshold.")
+    ap.add_argument("-l", "--layout", type = str, required = True,
+                    help = "Specify one of the two layout options 'shell' or 'random'.")
+
+    # Parse arguments. args is now an object containing all arguments added through the terminal. 
+    argument_parser = vars(ap.parse_args())
+    
+    # run main() function
+    main(argument_parser)
+    
 
 
 
-
-
-
-
-
+#spacytextblob
+#spacy==2.3.5
+#tqdm==4.56.2
 
 
